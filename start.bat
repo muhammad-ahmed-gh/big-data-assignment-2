32e946c6680efb8b0531820bf3e63b08aae85543
muhammad-ahmed-gh <muhammad.ahmed.connect@gmail.com>
2026-05-11 23:33:34 +0300

    FINAL

--- start.bat
+++ start.bat
@@ -5,22 +5,3 @@ docker compose up -d
 cd ../mapreduce
 docker cp mds.csv namenode:/
 docker cp out/artifacts/mapreduce_jar/mapreduce.jar namenode:/
-
-@REM prepare folders and insert data
-docker exec -it namenode hdfs dfs -mkdir /input
-docker exec -it namenode hdfs dfs -mkdir /output
-docker exec -it namenode hdfs dfs -put /mds.csv /input/
-
-@REM run mapreduce
-docker exec -it namenode hadoop jar /mapreduce.jar ARDriver /input /output
-
-@REM get output
-docker exec -it namenode mkdir /output
-docker exec -it namenode hdfs dfs -get /output/part-r-00000 /output
-docker exec -it namenode cat /output/part-r-00000
-docker cp namenode:/output/part-r-00000 .
-
-@REM compose down and return
-cd ../compose-images
-docker compose down
-cd ..
